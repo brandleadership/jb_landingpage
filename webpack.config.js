76bf915cd2338a0a4b6a048b6926b028ed2f3a99
Cécile Hefti <cecile.hefti@outlook.com>
2023-02-21 10:23:29 +0100

    feat: Consolidate webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,35 +8,6 @@ const {
     DesignType,
 } = require('@bsi-cx/design-build');
 
-// module.exports = WebpackConfigBuilder.fromConfigs(
-//     new BuildConfig()
-//         .withName('JB_LP')
-//         .withVersion('v3.0.1')
-//         .withDesignType(DesignType.LANDINGPAGE)
-//         .withTargetVersion(Version.CX_22_0)
-//         .withRootPath(path.resolve(__dirname, 'templates', 'landingpage'))
-//         .withPropertiesFilePath('properties.js')
-//         .withModules(
-//             new ModuleConfig().withName('main').withPath('main.js'),
-//             new ModuleConfig().withName('chart').withPath('chart.js'),
-//             new ModuleConfig()
-//                 .withName('chart_freeform')
-//                 .withPath('chart-freeform.js')
-//         )
-//         .withAdditionalFilesToCopy({
-//             from: path.resolve(__dirname, 'assets', 'img'), // copy from <project root>/files/**/*
-//             to: 'static/img', // copy to <output folder>/files/**/*
-//         })
-/* new BuildConfig()
-        .withName('website')
-        .withVersion('2.0.0')
-        .withDesignType(DesignType.WEBSITE)
-        .withTargetVersion(Version.CX_22_0)
-        .withRootPath(path.resolve(__dirname, 'templates', 'website'))
-        .withPropertiesFilePath('properties.js')
-        .withModules(new ModuleConfig().withName('main').withPath('main.js')) */
-//);
-
 const landingpageBuildConfig = new BuildConfig()
     .withName('JB_LP')
     .withVersion('v3.0.1')
@@ -76,5 +47,17 @@ module.exports = WebpackConfigBuilder.fromConfigs(
     landingpageBuildConfig
         .clone()
         .withName('JB-LP-en-cx-22.0')
-        .withPropertiesFilePath('lang-en.js')
+        .withPropertiesFilePath('lang-en.js'),
+    new BuildConfig()
+        .withName('website_JB_')
+        .withVersion('v2.0.0')
+        .withDesignType(DesignType.WEBSITE)
+        .withTargetVersion(Version.CX_22_0)
+        .withRootPath(path.resolve(__dirname, 'templates', 'website'))
+        .withPropertiesFilePath('properties.js')
+        .withModules(new ModuleConfig().withName('main').withPath('main.js'))
+        .withAdditionalFilesToCopy({
+            from: path.resolve(__dirname, 'assets', 'img'), // copy from <project root>/files/**/*
+            to: 'static/img', // copy to <output folder>/files/**/*
+        })
 );
