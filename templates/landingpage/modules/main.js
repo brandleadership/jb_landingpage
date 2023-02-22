f7079106115d773413241dd5f527b43fb75d14c1
Cécile Hefti <cecile.hefti@outlook.com>
2023-02-22 09:54:51 +0100

    feat: Move modules outside of lp folder

--- templates/landingpage/modules/main.js
+++ templates/landingpage/modules/main.js
@@ -1,5 +1,5 @@
 // import '../scripts/custom';
 // import '../scripts/plyr';
-import './chart';
-import './chart-default';
-import './chart-freeform';
+import '../../../modules/chart';
+import '../../../modules/chart-default';
+import '../../../modules/chart-freeform';
