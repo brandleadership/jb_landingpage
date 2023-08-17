8dbfb55bb07bfb1bce465344c0e8a3809775fff2
Cécile Hefti <cecile.hefti@outlook.com>
2023-08-17 11:55:35 +0200

    fix: Fix form behaviour

--- components/elements/rating-bar-form/index.js
+++ components/elements/rating-bar-form/index.js
@@ -5,20 +5,10 @@ module.exports = cx.contentElement
     .withIcon(Icon.SNIPPET)
     .withLabel('Rating Bar with Form')
     .withFile(require('./template.twig'))
-    // .withParts(
-    //     cx.part.plainText.withLabel('Heading'),
-    //     cx.part.plainText.withLabel('Description'),
-    //     cx.part.link.withLabel('Link Square Option 1'),
-    //     cx.part.link.withLabel('Label Option 1'),
-    //     cx.part.link.withLabel('Link Square Option 2'),
-    //     cx.part.link.withLabel('Label Option 2'),
-    //     cx.part.link.withLabel('Link Square Option 3'),
-    //     cx.part.link.withLabel('Label Option 3'),
-    //     cx.part.link.withLabel('Link Square Option 4'),
-    //     cx.part.link.withLabel('Label Option 4'),
-    //     cx.part.link.withLabel('Link Square Option 5'),
-    //     cx.part.link.withLabel('Label Option 5')
-    // )
+    .withParts(
+        cx.part.plainText.withLabel('Heading'),
+        cx.part.formattedText.withLabel('Description')
+    )
     .withDropzones(
         cx.dropzone
             .withDropzone('rating-bar-form-container')
